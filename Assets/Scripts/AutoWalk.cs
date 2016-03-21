906baa72a002a3bfd0efd195553bccdf9373e788
Nbalsh <brandon.bn.nelson@gmail.com>
2016-03-20 23:22:07 -0700

    First Commit, Hope it works

---
+++ Assets/Scripts/AutoWalk.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoWalk : MonoBehaviour 
+{
+	CardboardHead head;
+	Cardboard cardboard;
+	public ProgressBar[] progressBars;
+	public float speed = 20;
+	public float boostspeed = 80;
+	private bool goFaster = false;
+
+	public float totalBoostTime = 2.5f;
+	private float timeUntilBoostDone = 0.0f;
+
+	void Start () 
+	{
+		head = Camera.main.GetComponent<StereoController>().Head;
+		cardboard = head.GetComponentInParent<Cardboard> ();
+	}
+
+	void Update () 
+	{
+		// boost time?
+		if (cardboard.Triggered) {
+			for (var i = 0; i < progressBars.Length; i++) {
+				goFaster = progressBars[i].ActivateProgressBar();
+			}
+			if (goFaster) {
+				timeUntilBoostDone = Time.fixedTime + totalBoostTime;
+			}
+		}
+		if (timeUntilBoostDone > Time.fixedTime) {
+			speed = boostspeed;
+		} else {
+			speed = speed;
+		}
+
+		// for aiming where head is aiming
+		Vector3 direction = new Vector3(head.transform.forward.x, 0, head.transform.forward.z).normalized * speed * Time.deltaTime;
+		Quaternion rotation = Quaternion.Euler(new Vector3(0, -transform.rotation.eulerAngles.y, 0));
+		transform.Translate(rotation * direction);
+		// move
+		transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+		speed = 20;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Wall") {
+			// hit wall, go back to middle
+			gameObject.transform.position = new Vector3 (0, 1, 0);
+		}
+	}
+
+}
---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour{
+	Image image;
+	public float timeToFill = 7.0f;
+	private bool syncFillBar = false;
+	private float syncTimeUntilFillAgain = 0.0f;
+
+	void Start()
+	{
+		image = GetComponent<Image>();
+		image.fillAmount = 1;
+	}
+
+	void Update()
+	{
+		if (syncFillBar)
+		{
+			image.fillAmount = Mathf.MoveTowards(image.fillAmount, 1f, Time.deltaTime / timeToFill);
+		} else if (syncTimeUntilFillAgain < Time.deltaTime)
+		{
+			syncFillBar = false;
+		}
+	}
+
+	public bool ActivateProgressBar()
+	{
+		if (syncTimeUntilFillAgain < Time.fixedTime)
+		{
+			syncTimeUntilFillAgain = Time.fixedTime + timeToFill;
+			image.fillAmount = 0;
+			syncFillBar = true;
+			return true;
+		}
+		return false;
+	}
+
+}
